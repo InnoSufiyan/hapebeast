d7d8313091796897ad5ebf5ce01bce24264ce61e
inno sufiyan <innosufiyan@gmail.com>
2022-01-13 16:06:00 +0500

    roadMap_2

--- src/components/RoadMap/index.js
+++ src/components/RoadMap/index.js
@@ -11,6 +11,14 @@ function RoadMap({
   return (
     <Wrap bgImage={backgroundImg} checkImage={imagey} colory={color}>
       <Fade bottom>
+        <img style={{
+          position: "absolute",
+          bottom: -200,
+          right: "-80px",
+          width: "1100px",
+          height: "1100px",
+
+        }} src="/images/Metaversity 5_2.png" />
         <ItemText>
           <h1>
             Road Map
@@ -21,16 +29,55 @@ function RoadMap({
         </ItemText>
         <MainDiv>
           <FirstDiv>
-            <h3>Monkeys are Born</h3>
-            <p>
-            The community always comes first. The goal with this project is to provide as much value as possible so that you get the greatest return on your investment. We will be announcing the drop release date once we are fully satisfied with all parts of the project. We want to make sure we provide as much as we can to the community even before our release. Once we are satisfied, the beautiful 3D Metaversity Monkeys will come to life.
-            </p>
+            <img style={{
+              position: "absolute",
+              top: -200,
+              left: -200,
+
+            }} src="/images/sun_.png" />
+            <img style={{
+              position: "absolute",
+              bottom: 0,
+              left: -200,
+
+            }} src="/images/img13.png" />
+            <FirstSection>
+              <h3>Monkeys are Born</h3>
+              <p>
+                The community always comes first. The goal with this project is to provide as much value as possible so that you get the greatest return on your investment. We will be announcing the drop release date once we are fully satisfied with all parts of the project. We want to make sure we provide as much as we can to the community even before our release. Once we are satisfied, the beautiful 3D Metaversity Monkeys will come to life.
+              </p>
+            </FirstSection>
+            <SecondSection>
+              <h3>Metaverse University</h3>
+              <p>
+                We are committed to helping the community to the fullest. We will be bringing together a community of hustlers through various media. This will be offered exclusively to The Metaverse University students where each NFT holder will gain full access to private seminars, events, courses, mentorship and even university parties!
+              </p>
+            </SecondSection>
           </FirstDiv>
           <SecondDiv>
             <img src="/images/roadmap 2.png" />
           </SecondDiv>
           <ThirdDiv>
+            <img style={{
+              position: "absolute",
+              top: -450,
+              right: -200,
 
+            }} src="/images/img15 copy.png" />
+            
+
+            <FirstSection>
+              <h3>Monkey Madness</h3>
+              <p>
+                Our goal is to show that an investment in a monkey will be extremely profitable for you even in the long term. As we continue to expand, we plan on hosting events across the world and doing promotions with massive celebrities. You will be part of an exclusive club (or should we say University). Metaversity Merch and Private Party: We will release our first merch collection of the Metaversity Monkeys that will be available only for the holder. To celebrate the achievement of our goals, we are going to organize a private party for our holders, where the community chooses the location.
+              </p>
+            </FirstSection>
+            <SecondSection>
+              <h3>MetaverCity</h3>
+              <p>
+                We have already purchased a small plot of land in Decentraland for our community. As our community continues to grow, we will expand our virtual real estate in various metaverses and build the MetaverCity. Eventually we will begin hosting events in the Metaverse.
+              </p>
+            </SecondSection>
           </ThirdDiv>
         </MainDiv>
       </Fade>
@@ -42,11 +89,13 @@ export default RoadMap;
 
 const Wrap = styled.div`
   width: 100vw;
-  height: 250vh;
+  height: 200vh;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-image: ${(props) => props.checkImage ? `url("/images/${props.bgImage}")` : `radial-gradient(${props.colory}, ${props.colory} , ${props.colory})`};
+  position: relative;
+  overflow-y: hidden;
 `;
 
 const ItemText = styled.div`
@@ -70,14 +119,32 @@ const ItemText = styled.div`
 
 const MainDiv = styled.div`
   margin: 60px 0 0 0;
+  display: flex;
+  justify-content: center;
+  gap: 30px;
 `
 const FirstDiv = styled.div`
+  max-width: 400px;
+  color: white;
+  margin: 30px 0 0 0;
+  position: relative;
 `
 const SecondDiv = styled.div`
   
 `
 const ThirdDiv = styled.div`
-`
+  max-width: 400px;
+  color: white;
+  margin: 200px 0 0 0;
+  position: relative;
+  `
 
 
+const FirstSection = styled.div`
+
+    
+  `
+const SecondSection = styled.div`
+    margin: 250px 0 0 0;
+  `
 const Buttons = styled.div``;
